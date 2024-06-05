e7ac093c7c5d1232f6369284e26754fad6229947
Izzat Aliyev <izzat@izzats-mbp.speedport.ip>
2024-06-06 00:56:22 +0200

    [update] added rooms and users

--- RTChat.Api/Hubs/ChatHub.cs
+++ RTChat.Api/Hubs/ChatHub.cs
@@ -1,11 +1,34 @@
+using System.Collections.Concurrent;
 using Microsoft.AspNetCore.SignalR;
 
 namespace RTChat.Api.Hubs;
 
+public record User(string Name, string Room);
+public record Message(string User, string Text);
+
 public class ChatHub : Hub
 {
-    public async Task SendMessage(string message)
+    private static readonly ConcurrentDictionary<string, User> users = new();
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
     {
-        await  Clients.All.SendAsync("ReceiveMessage", message);
+        if (users.TryGetValue(Context.ConnectionId, out var user))
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, user.Room);
+            await Clients.Group(user.Room).SendAsync("UserLeft", user.Name);
+        }
+    }
+    
+    public async Task JoinRoom(string userName, string roomName)
+    {
+        users.TryAdd(Context.ConnectionId, new User(userName, roomName));
+        await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+        await Clients.Group(roomName).SendAsync("UserJoined", userName);
+    }
+    
+    public async Task SendMessageToRoom(string roomName, string content)
+    {
+        var message = new Message(users[Context.ConnectionId].Name, content);
+        await Clients.Group(roomName).SendAsync("ReceiveMessage", message);
     }
 }
